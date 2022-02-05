bc1b85db5eed57d5729b118b3edd9a4667a31461
Andrei Vilkevich <andreivilkevich@disroot.org>
2022-02-05 19:27:49 +0300

    Add the configuration for tym

---
+++ .config/tym/config.lua
@@ -0,0 +1,9 @@
+local tym = require('tym')
+
+tym.set('font', 'Iosevka Term Medium Extended 12')
+tym.set_config {
+  title = 'Tym',
+  cursor_shape = 'underline',
+  padding_horizontal = 16,
+  padding_vertical = 16,
+}
---
+++ .config/tym/theme.lua
@@ -0,0 +1,46 @@
+local bg        = '#00010a'
+local fg        = '#e5e9f0'
+
+local black      = '#0f111a'
+local red       = '#f07178'
+local green     = '#80cbc4'
+local yellow    = '#ffcb6b'
+local blue      = '#82aaff'
+local magenta   = '#c792ea'
+local cyan      = '#80cbc4'
+local white     = '#e5e9f0'
+
+return {
+  color_background              = bg,
+  color_foreground              = fg,
+  color_window_background       = bg,
+  color_bold                    = fg,
+  color_cursor                  = red,
+  color_cursor_foreground       = bg,
+  color_highlight               = cyan,
+  color_highlight_foreground    = bg,
+  
+  color_0       = bg,
+  color_8       = black,
+
+  color_1       = red,
+  color_9       = red,
+  
+  color_2       = green,
+  color_10      = green,
+  
+  color_3       = yellow,
+  color_11      = yellow,
+  
+  color_4       = blue,
+  color_12      = blue,
+  
+  color_5       = magenta,
+  color_13      = magenta,
+  
+  color_6       = cyan,
+  color_13      = cyan,
+  
+  color_7       = white,
+  color_15      = white,
+}
